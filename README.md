835444ab50ed5b54cef9670318855a8abeaaf40c
DesarrolloAY <ymollob@gmail.com>
2025-08-20 01:45:48 -0500

    Act1_Creacion

---
+++ README.md
@@ -0,0 +1,3 @@
+# guia2_actividad1
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/guia2_actividad1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.guia2_actividad1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
